849c6e0a8a0c8f6cad73fe434f8c30967cbc41d0
kha <khalinux@naver.com>
2024-08-09 00:33:27 +0900

    5'th commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "test.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    test tobj;
+
+
 }
 
 MainWindow::~MainWindow()
